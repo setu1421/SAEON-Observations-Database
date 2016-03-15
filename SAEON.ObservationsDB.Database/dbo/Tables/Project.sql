98f8bf344252060b30a729b36e9ce9dc0e6c70b7
Tim Parker-Nance <tim@nimbusservices.co.za>
2016-03-15 09:31:14 +0000

    git-tfs-id: [https://nimbusservices.visualstudio.com]$/SAEON/Observations Database;C829

---
+++ SAEON.ObservationsDB.Database/dbo/Tables/Project.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Project]
+(
+    [ID] UNIQUEIDENTIFIER CONSTRAINT [DF_Project_ID] DEFAULT newid(), 
+    [Code] VARCHAR(50) NOT NULL, 
+    [Name] VARCHAR(150) NOT NULL, 
+    [Description] VARCHAR(5000) NULL,
+    [UserId] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_Project] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [UX_Project_Code] UNIQUE ([Code] ASC),
+    CONSTRAINT [UX_Project_Name] UNIQUE ([Name] ASC),
+    CONSTRAINT [FK_Project_aspnet_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[aspnet_Users] ([UserId]),
+)
