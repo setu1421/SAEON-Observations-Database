8d9963b51bb7d926392bb6b607868500cf80636e
Tim Parker-Nance <tim@nimbusservices.co.za>
2016-03-15 10:26:22 +0000

    git-tfs-id: [https://nimbusservices.visualstudio.com]$/SAEON/Observations Database;C835

---
+++ SAEON.ObservationsDB.Database/dbo/Tables/Site_Organisation.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Site_Organisation]
+(
+    [ID] UNIQUEIDENTIFIER CONSTRAINT [DF_Site_Organisation_ID] DEFAULT newid(), 
+    [SiteID] UNIQUEIDENTIFIER NOT NULL, 
+    [OrganisationID] UNIQUEIDENTIFIER NOT NULL, 
+    [OrganisationRoleID] UNIQUEIDENTIFIER NOT NULL, 
+    [UserId] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_Site_Organisation] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_Site_Organisation_Site] FOREIGN KEY ([SiteID]) REFERENCES [dbo].[Site] ([ID]),
+    CONSTRAINT [FK_Site_Organisation_Organisation] FOREIGN KEY ([OrganisationID]) REFERENCES [dbo].[Organisation] ([ID]),
+    CONSTRAINT [FK_Site_Organisation_OrganisationRole] FOREIGN KEY ([OrganisationRoleID]) REFERENCES [dbo].[OrganisationRole] ([ID]),
+    CONSTRAINT [FK_Site_Organisation_aspnet_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[aspnet_Users] ([UserId]),
+    CONSTRAINT [UX_Site_Organisation_SiteIDOrganisation] UNIQUE ([SiteID],[OrganisationID])
+)
