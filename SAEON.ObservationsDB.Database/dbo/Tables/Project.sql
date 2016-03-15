8f56386c73c73e912cfaaf322974edd6630316e7
Tim Parker-Nance <tim@nimbusservices.co.za>
2016-03-15 07:50:55 +0000

    git-tfs-id: [https://nimbusservices.visualstudio.com]$/SAEON/Observations Database;C821

---
+++ SAEON.ObservationsDB.Database/dbo/Tables/Project.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Project]
+(
+    [ID] UNIQUEIDENTIFIER NOT NULL DEFAULT newid(), 
+    [Code] VARCHAR(50) NOT NULL, 
+    [Name] VARCHAR(150) NOT NULL, 
+    [Description] VARCHAR(5000) NULL,
+    [UserId] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_Project] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [IX_Project_Code] UNIQUE ([Code] ASC),
+    CONSTRAINT [IX_Project_Name] UNIQUE ([Name] ASC),
+    CONSTRAINT [FK_Project_aspnet_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[aspnet_Users] ([UserId]),
+)
---
+++ SAEON.ObservationsDB.Database/dbo/Tables/Site.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Site]
+(
+    [ID] UNIQUEIDENTIFIER NOT NULL DEFAULT newid(), 
+    [Code] VARCHAR(50) NOT NULL, 
+    [Name] VARCHAR(150) NOT NULL, 
+    [Description] VARCHAR(5000) NULL,
+    [UserId] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_Site] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [IX_Site_Name] UNIQUE ([Name] ASC),
+    CONSTRAINT [IX_Site_Code] UNIQUE ([Code] ASC),
+    CONSTRAINT [FK_Site_aspnet_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[aspnet_Users] ([UserId]),
+)
+
