5bf5b2c30daf1d19acfec3f6565655d982cd0685
Tim Parker-Nance <tim@nimbusservices.co.za>
2016-03-22 12:48:13 +0000

    git-tfs-id: [https://nimbusservices.visualstudio.com]$/SAEON/Observations Database;C863

--- SAEON.ObservationsDB.WebSite/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace SAEON.ObservationsDB.WebSite.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- SAEON.ObservationsDB.WebSite/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SAEON.ObservationsDB.WebSite.Global" Language="C#" %>
