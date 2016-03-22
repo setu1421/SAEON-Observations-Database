ca1c2046a40941344071ff2307a5d7106db86230
Tim Parker-Nance <tim@nimbusservices.co.za>
2016-03-22 12:02:36 +0000

    git-tfs-id: [https://nimbusservices.visualstudio.com]$/SAEON/Observations Database;C857

---
+++ Observations.Data/Extensions/Status.cs
@@ -0,0 +1,22 @@
+﻿using SubSonic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAEON.ObservationsDB.Data
+{
+    public partial class Status : ActiveRecord<Status>, IActiveRecord, IRecordBase
+    {
+        public const string DateInvalid = "3ccec392-e437-4b45-a2b2-dba4d92a1db5";
+        public const string TimeInvalid = "30c1bef8-37a7-4e2f-8930-48a5cdb9a9af";
+        public const string OfferingInvalid = "94f865e2-c366-4313-980d-f6ec12d60e83";
+        public const string ValueInvalid = "27f857ac-61b0-4c4a-86ab-777246d42e2b";
+        public const string TransformValueInvalid = "e9cdf5dc-badb-42fe-92a3-f8f239fb2d7e";
+        public const string UOMInvalid = "d9e0289e-02d5-4ee2-8134-8516476aa31a";
+        public const string SensorProcedureNotFound = "dda15e09-c652-400e-9111-ebfc192363ff";
+        public const string BatchRetracted = "302c69b6-f9d4-4d9f-82bb-a61cfc5dd80b";
+    }
+
+}
---
+++ Observations.Data/Extensions/TransformationType.cs
@@ -0,0 +1,16 @@
+﻿using SubSonic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAEON.ObservationsDB.Data
+{
+    public partial class TransformationType : ActiveRecord<TransformationType>, IActiveRecord, IRecordBase
+    {
+        public const string RatingTable = "538d1697-c817-4644-8b0d-5100a4acc451";
+        public const string QualityControlValues = "9ca36c10-cbad-4862-9f28-591acab31237";
+        public const string CorrectionValues = "4b850c72-d15f-4bf4-a99a-a5bd3de13349";
+    }
+}
