3b272509bb0610cd4c90a8dedb592a2308fb4e87
Tim Parker-Nance <tim@nimbusservices.co.za>
2016-04-12 11:35:55 +0000

    git-tfs-id: [https://nimbusservices.visualstudio.com]$/SAEON/Observations Database;C907

---
+++ SAEON.ObservationsDB.Database/dbo/Tables/Station_Organisation.sql
@@ -0,0 +1,26 @@
+﻿--> Added 20160406 TimPN
+CREATE TABLE [dbo].[Station_Organisation]
+(
+    [ID] UNIQUEIDENTIFIER CONSTRAINT [DF_Station_Organisation_ID] DEFAULT newid(), 
+    [StationID] UNIQUEIDENTIFIER NOT NULL, 
+    [OrganisationID] UNIQUEIDENTIFIER NOT NULL, 
+    [OrganisationRoleID] UNIQUEIDENTIFIER NOT NULL,
+    [StartDate] DATETIME NULL,
+    [EndDate] DATETIME NULL,
+    [UserId] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_Station_Organisation] PRIMARY KEY CLUSTERED ([ID]),
+    CONSTRAINT [FK_Station_Organisation_Station] FOREIGN KEY ([StationID]) REFERENCES [dbo].[Station] ([ID]),
+    CONSTRAINT [FK_Station_Organisation_Organisation] FOREIGN KEY ([OrganisationID]) REFERENCES [dbo].[Organisation] ([ID]),
+    CONSTRAINT [FK_Station_Organisation_OrganisationRole] FOREIGN KEY ([OrganisationRoleID]) REFERENCES [dbo].[OrganisationRole] ([ID]),
+    CONSTRAINT [FK_Station_Organisation_aspnet_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[aspnet_Users] ([UserId]),
+    CONSTRAINT [UX_Station_Organisation_StationIDOrganisationIDOrganisationRoleID] UNIQUE ([StationID],[OrganisationID],[OrganisationRoleID])
+)
+GO
+CREATE INDEX [IX_Station_Organisation_StationID] ON [dbo].[Station_Organisation] ([StationID])
+GO
+CREATE INDEX [IX_Station_Organisation_OrganisationID] ON [dbo].[Station_Organisation] ([OrganisationID])
+GO
+CREATE INDEX [IX_Station_Organisation_OrganisationRoleID] ON [dbo].[Station_Organisation] ([OrganisationRoleID])
+GO
+CREATE INDEX [IX_Station_Organisation_UserId] ON [dbo].[Station_Organisation] ([UserId])
+--< Added 20160406 TimPN
