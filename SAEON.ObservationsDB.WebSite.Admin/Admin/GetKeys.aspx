b829585675d75d22aa06180ac423e883481e5d39
Tim Parker-Nance <tim@nimbusservices.co.za>
2016-05-08 09:33:35 +0000

    git-tfs-id: [https://nimbusservices.visualstudio.com]$/SAEON/Observations Database;C965

---
+++ SAEON.ObservationsDB.WebSite.Admin/Admin/GetKeys.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="GetKeys.aspx.cs" Inherits="Admin_GetKeys" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <table style="width:100%;">
+        <tr>
+            <td>Key</td>
+            <td>Value</td>
+        </tr>
+        <tr>
+            <td>Decryption</td>
+            <td><asp:Label ID="Decryption" runat="server" Text=""></asp:Label></td>
+        </tr>
+        <tr>
+            <td>DecryptionKey</td>
+            <td><asp:Label ID="DecryptionKey" runat="server" Text=""></asp:Label></td>
+        </tr>
+        <tr>
+            <td>DecryptionKeyValue</td>
+            <td><asp:Label ID="DecryptionKeyValue" runat="server" Text=""></asp:Label></td>
+        </tr>
+        <tr>
+            <td>Validation</td>
+            <td><asp:Label ID="Validation" runat="server" Text=""></asp:Label></td>
+        </tr>
+        <tr>
+            <td>ValidationAlgorithm</td>
+            <td><asp:Label ID="ValidationAlgorithm" runat="server" Text=""></asp:Label></td>
+        </tr>
+        <tr>
+            <td>ValidationKey</td>
+            <td><asp:Label ID="ValidationKey" runat="server" Text=""></asp:Label></td>
+        </tr>
+        <tr>
+            <td>ValidationKeyValue</td>
+            <td><asp:Label ID="ValidationKeyValue" runat="server" Text=""></asp:Label></td>
+        </tr>
+    </table>
+</asp:Content>
+
---
+++ SAEON.ObservationsDB.WebSite.Admin/Admin/GetKeys.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_GetKeys : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        MachineKeySection section = (MachineKeySection)ConfigurationManager.GetSection("system.web/machineKey");
+        Decryption.Text = section.Decryption;
+        DecryptionKey.Text = section.DecryptionKey;
+        Validation.Text = section.Validation.ToString();
+        ValidationAlgorithm.Text = section.ValidationAlgorithm;
+        ValidationKey.Text = section.ValidationKey;
+        var machineKey = typeof(MachineKeySection).GetMethods(BindingFlags.Static | BindingFlags.NonPublic).Single(a => a.Name == "GetApplicationConfig").Invoke(null, new object[0]);
+
+        var type = Assembly.Load("System.Web, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a").GetTypes().Single(a => a.Name == "MachineKeyMasterKeyProvider");
+
+        var instance = type.Assembly.CreateInstance(
+            type.FullName, false,
+            BindingFlags.Instance | BindingFlags.NonPublic,
+            null, new object[] { machineKey, null, null, null, null }, null, null);
+
+        var validationKey = type.GetMethod("GetValidationKey").Invoke(instance, new object[0]);
+        var key = (byte[])validationKey.GetType().GetMethod("GetKeyMaterial").Invoke(validationKey, new object[0]);
+
+        DecryptionKeyValue.Text = BitConverter.ToString(GetKey(instance, "GetEncryptionKey")).Replace("-", "");
+        ValidationKeyValue.Text = BitConverter.ToString(GetKey(instance, "GetValidationKey")).Replace("-", "");
+    }
+
+    public static byte[] GetKey(object provider, string name)
+    {
+        var validationKey = provider.GetType().GetMethod(name).Invoke(provider, new object[0]);
+        return (byte[])validationKey.GetType().GetMethod("GetKeyMaterial").Invoke(validationKey, new object[0]);
+    }
+}
