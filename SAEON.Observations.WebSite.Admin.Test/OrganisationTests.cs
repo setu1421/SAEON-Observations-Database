84748063ea8860be609aa2fea5d4d0375423bc01
Tim Parker-Nance <tim@nimbusservices.co.za>
2016-06-27 14:21:48 +0000

    git-tfs-id: [https://nimbusservices.visualstudio.com]$/SAEON/Observations Database;C1023

---
+++ SAEON.Observations.WebSite.Admin.Test/OrganisationTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace SAEON.Observations.WebSite.Admin.Test
+{
+    [TestClass]
+    public class OrganisationTests
+    {
+        static IWebDriver driverChrome;
+
+        [AssemblyInitialize]
+        public static void Setup(TestContext context)
+        {
+            driverChrome = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SAEON.Observations.WebSite.Admin.Test/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace SAEON.Observations.WebSite.Admin.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //static IWebDriver driverFirefox;
+        static IWebDriver driverChrome;
+
+        [AssemblyInitialize]
+        public static void Setup(TestContext context)
+        {
+            //driverFirefox = new FirefoxDriver();
+            driverChrome = new ChromeDriver();
+        }
+
+        //[TestMethod]
+        //public void TestFirefox()
+        //{
+        //    driverFirefox.Navigate().GoToUrl("https://www.google.co.za");
+        //    driverFirefox.FindElement(By.Id("lst-ib")).SendKeys("Selenium");
+        //    driverFirefox.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
+        //}
+
+        [TestMethod]
+        public void TestChrome()
+        {
+            driverChrome.Navigate().GoToUrl("https://www.google.co.za");
+            driverChrome.FindElement(By.Id("lst-ib")).SendKeys("Selenium");
+            driverChrome.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
+        }
+
+    }
+}
