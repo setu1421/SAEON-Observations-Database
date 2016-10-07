ffe4709078d24ba23f9ec78821c371e19db75829
Tim Parker-Nance <tim@nimbusservices.co.za>
2016-10-07 15:44:04 +0200

    Database version 2.0.12
    Dropped filtered indexes on Observation.Comment

---
+++ SQL/2.0.12/Changes.sql
@@ -0,0 +1,11 @@
+﻿PRINT 'Dropping IX_Observation_Comment'
+GO
+DROP INDEX [IX_Observation_Comment] ON [dbo].[Observation]
+PRINT 'Dropping IX_Observation_Comment_Null'
+GO
+DROP INDEX [IX_Observation_Comment_Null] ON [dbo].[Observation]
+PRINT 'Creating IX_Observation_Comment'
+GO
+CREATE INDEX [IX_Observation_Comment] ON [dbo].[Observation] ([Comment])
+PRINT 'Done'
+GO
