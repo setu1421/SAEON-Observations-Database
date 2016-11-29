f99d368ed2deb0496a17c5488982f08d0fc87142
Tim Parker-Nance <tim@nimbusservices.co.za>
2016-11-29 20:27:15 +0200

    Tweaks

--- SQL/2.0.17/Observations.2.0.17.publish.sql
+++ SQL/2.0.17/Observations.2.0.17.publish.sql
@@ -564,6 +564,59 @@ GO
 PRINT N'Refreshing [dbo].[vObservationRoles]...';
 
 
+PRINT N'Altering [dbo].[vObservationRoles]...';
+
+
+GO
+--> Changed 2.0.3 20160503 TimPN
+--Renamed SensorProcedure to Sensor
+--< Changed 2.0.3 20160503 TimPN
+ALTER VIEW [dbo].[vObservationRoles]
+AS
+--> Changed 2.0.16 20161107 TimPN
+----> Changed 2.0.3 20160421 TimPN
+----SELECT     vo.ID, vo.SensorID, vo.PhenonmenonOfferingID, vo.PhenonmenonUOMID, vo.UserId, vo.RawValue, vo.DataValue, vo.ImportBatchID, vo.ValueDate, 
+--SELECT     vo.ID, vo.SensorID, vo.PhenomenonOfferingID, vo.PhenomenonUOMID, vo.UserId, vo.RawValue, vo.DataValue, vo.ImportBatchID, vo.ValueDate, 
+----> Changed 2.0.3 20160421 TimPN
+--                      vo.spCode, vo.spDesc, vo.spName, vo.spURL, vo.DataSchemaID, vo.DataSourceID, vo.PhenomenonID, vo.StationID, vo.phName, vo.stName, vo.dsName, 
+--                      vo.dschemaName, vo.offName, vo.offID, vo.psName, vo.psID, vo.orgName, vo.orgID, vo.uomUnit, vo.uomSymbol, vo.UserName,
+--                      dr.DataSourceID AS Expr2, dr.DateStart, dr.DateEnd,
+--                      ur.UserId AS Expr5, vo.Comment
+--FROM         dbo.vObservation AS vo 
+--INNER JOIN 
+--(
+-- SELECT dr.DataSourceID,
+--        ur.UserId,
+--        MIN(dr.DateStart) DateStart,
+--        MAX(dr.DateEnd) DateEnd
+-- FROM DataSourceRole dr
+-- INNER JOIN    dbo.aspnet_Roles AS ar ON dr.RoleId = ar.RoleId INNER JOIN
+--               dbo.aspnet_UsersInRoles AS ur ON ar.RoleId = ur.RoleId
+-- GROUP By dr.DataSourceID,ur.UserId
+--) dr
+--ON vo.DataSourceID = dr.DataSourceID 
+--AND vo.ValueDate >= dr.DateStart AND vo.ValueDate <= dr.DateEnd
+--INNER JOIN aspnet_Users ur
+-- ON dr.UserId = ur.UserId
+	Select
+		vObservation.*, DataSourceRoles.RoleUserId
+	from
+		vObservation
+		inner join
+        (
+			Select
+				dsr.DataSourceID, aspnet_UsersInRoles.UserId RoleUserId, Min(dsr.DateStart) DateStart, Max(dsr.DateEnd) DateEnd
+			from
+				DataSourceRole dsr
+			inner join aspnet_UsersInRoles
+				on (dsr.RoleId = aspnet_UsersInRoles.RoleId)
+			group by
+				dsr.DataSourceID, aspnet_UsersInRoles.UserId
+		) DataSourceRoles
+		on (vObservation.DataSourceID = DataSourceRoles.DataSourceID) and
+		   (vObservation.ValueDate >= DataSourceRoles.DateStart) and (vObservation.ValueDate <= DataSourceRoles.DateEnd)
+	  
+--< Changed 2.0.16 20161107 TimPN
 GO
 EXECUTE sp_refreshsqlmodule N'[dbo].[vObservationRoles]';
 
