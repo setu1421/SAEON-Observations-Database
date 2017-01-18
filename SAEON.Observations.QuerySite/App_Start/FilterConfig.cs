20585f7ecd4e22a24c0ee2df4ab3f77f48491f81
Tim Parker-Nance <tim@nimbusservices.co.za>
2017-01-18 11:47:48 +0200

    Facebooak and Google external logins

--- SAEON.Observations.QuerySite/App_Start/FilterConfig.cs
+++ SAEON.Observations.QuerySite/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace SAEON.Observations.QuerySite
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            //filters.Add(new RequireHttpsAttribute());
         }
     }
 }
