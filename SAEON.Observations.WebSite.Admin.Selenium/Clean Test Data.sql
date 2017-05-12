2ae3ceb51a7f319c88e9a6bad965223878163cce
Tim Parker-Nance <tim@nimbusservices.co.za>
2017-05-12 14:02:05 +0200

    Selenium Automated UI tests updated

--- SAEON.Observations.WebSite.Admin.Selenium/Clean Test Data.sql
+++ SAEON.Observations.WebSite.Admin.Selenium/Clean Test Data.sql
@@ -1,3 +1,5 @@
+use [ObservationsTest]
+Delete from [Instrument_Sensor] where SensorID = (Select ID from Sensor where Code like '_Test_%')
 Delete from [Sensor] where Code like '_Test_%'
 Delete [Organisation_Instrument] from [Organisation_Instrument] inner join [Instrument] on ([Organisation_Instrument].InstrumentID = [Instrument].ID) where [Instrument].Code like '_Test_%'
 Delete [Station_Instrument] from [Station_Instrument] inner join [Instrument] on ([Station_Instrument].InstrumentID = [Instrument].ID) where [Instrument].Code like '_Test_%'
@@ -5,7 +7,7 @@ Delete from [Instrument] where Code like '_Test_%'
 Delete [Organisation_Station] from [Organisation_Station] inner join [Station] on ([Organisation_Station].StationID = [Station].ID) where [Station].Code like '_Test_%'
 Delete [Project_Station] from [Project_Station] inner join [Station] on ([Project_Station].StationID = [Station].ID) where [Station].Code like '_Test_%'
 Delete [Organisation_Site] from [Organisation_Site] inner join [Site] on ([Organisation_Site].SiteID = [Site].ID) where [Site].Code like '_Test_%'
-Update [Station] set SiteID = null from [Station] inner join [Site] on ([Station].SiteID = [Site].ID) where [Site].Code like '_Test_%' 
+--Update [Station] set SiteID = null from [Station] inner join [Site] on ([Station].SiteID = [Site].ID) where [Site].Code like '_Test_%' 
 Delete from [Station] where Code like '_Test_%'
 Delete from [Site] where Code like '_Test_%'
 Delete from [Project] where Code like '_Test_%'
