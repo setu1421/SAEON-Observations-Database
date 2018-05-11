b58c34b7521f4cb9b543f3cc018a6b5aa6eb03e0
Tim Parker-Nance <timpn@saeon.ac.za>
2018-05-11 10:50:42 +0200

    Data version 2.0.38

--- SQL/2.0.38/PostProcess.sql
+++ SQL/2.0.38/PostProcess.sql
@@ -1,7 +1,7 @@
 Insert Into ImportBatchSummary
   (ImportBatchID, SensorID, PhenomenonOfferingID, PhenomenonUOMID, Count, Minimum, Maximum, Average, StandardDeviation, Variance)
 Select
-  ImportBatchID, SensorID, PhenomenonOfferingID, PhenomenonUOMID, COUNT(ImportBatchID) Count, MIN(DataValue) Minimum, MAX(DataValue) Maximum, AVG(DataValue) Average, STDEV(DataValue) StandardDeviation, VAR(DataValue) Variance
+  ImportBatchID, SensorID, PhenomenonOfferingID, PhenomenonUOMID, COUNT(DataValue) Count, MIN(DataValue) Minimum, MAX(DataValue) Maximum, AVG(DataValue) Average, STDEV(DataValue) StandardDeviation, VAR(DataValue) Variance
 from
   Observation
 group by
