7d98d0ff02fac07241887cdfa42914997f206ebb
Tim Parker-Nance <timpn@saeon.ac.za>
2018-07-04 16:13:55 +0200

    Cleanup

--- SAEON.Observations.WebAPI.v2/Home/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SAEON.Observations.WebAPI.UI
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
