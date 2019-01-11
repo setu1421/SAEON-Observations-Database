27d88ad5b5fef94f7662c53d9ee82afeaeb7e833
Tim Parker-Nance <timpn@saeon.ac.za>
2019-01-11 12:41:49 +0200

    Package updates

--- SAEON.Observations.WebAPI/Properties/AssemblyInfo.cs
+++ SAEON.Observations.WebAPI/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("South African Environmental Observation Network (SAEON)")]
 [assembly: AssemblyProduct("SAEON Observations Database")]
-[assembly: AssemblyCopyright("Copyright © 2017-2018 South African Environmental Observation Network (SAEON)")]
+[assembly: AssemblyCopyright("© 2016-2019 South African Environmental Observation Network (SAEON)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
