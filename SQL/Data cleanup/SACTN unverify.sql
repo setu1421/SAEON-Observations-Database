fc5ca9e8e0cff28e7839337077984e555ace4f13
Tim Parker-Nance (SAEON) <timpn@saeon.ac.za>
2021-09-02 13:20:28 +0200

    Data version 2.0.69

---
+++ SQL/Data cleanup/SACTN unverify.sql
@@ -0,0 +1,32 @@
+--Select
+--  Count(*)
+--from
+--  vImportBatchSummary
+--  inner join Observation
+--    on (Observation.ImportBatchID = vImportBatchSummary.ImportBatchID) and
+--	   (Observation.SensorID = vImportBatchSummary.SensorID) and
+--	   (Observation.PhenomenonOfferingID = vImportBatchSummary.PhenomenonOfferingID) and
+--	   (Observation.PhenomenonUOMID = vImportBatchSummary.PhenomenonUOMID)
+--where 
+--  (StationCode like 'SACTN%') and
+--  (StationCode not like '% SAEON') and
+--  (StatusID is null)
+
+Declare @StatusID UniqueIdentifier = (Select ID from Status where Code = 'QA-97')
+Declare @StatusReasonID UniqueIdentifier = (Select ID from StatusReason where Code = 'QAR-38')
+Update
+  Observation
+set
+  StatusID = @StatusID,
+  @StatusReasonID = @StatusReasonID
+from
+  vImportBatchSummary
+  inner join Observation
+    on (Observation.ImportBatchID = vImportBatchSummary.ImportBatchID) and
+	   (Observation.SensorID =vImportBatchSummary.SensorID) and
+	   (Observation.PhenomenonOfferingID = vImportBatchSummary.PhenomenonOfferingID) and
+	   (Observation.PhenomenonUOMID = vImportBatchSummary.PhenomenonUOMID)
+where
+  (StationCode like 'SACTN%') and
+  (StationCode not like '% SAEON') and
+  (StatusID is null)
